c2124e509c94f5b01a1955cbdc851a69f1d0392f
Cristhian Castro <ccastrove.05@gmail.com>
2026-02-13 23:19:25 -0500

    Inicialización del proyecto CI/CD - Script de comisiones

---
+++ app/script_comisiones.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from datetime import datetime
+
+def generar_reporte_comisiones():
+    print("Iniciando proceso de cálculo de comisiones...")
+
+    # Simulación de datos de ventas
+    data = {
+        "vendedor": ["Ana", "Luis", "Carlos", "Sofia", "Cristhian", "Jose", "Said", "Lucy","Teresa", "Fernando"],
+        "ventas": [1000, 1500, 2000, 1800, 2500, 1500, 3500, 1250, 5000, 3200]
+    }
+
+    df = pd.DataFrame(data)
+
+    # Cálculo de comisión (10%)
+    df["comision"] = df["ventas"] * 0.10
+
+    # Fecha de generación
+    fecha = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    nombre_archivo = f"reporte_comisiones_{fecha}.csv"
+
+    # Guardar archivo CSV
+    df.to_csv(nombre_archivo, index=False)
+
+    print(f"Reporte generado correctamente: {nombre_archivo}")
+    print("Proceso finalizado exitosamente.")
+
+
+if __name__ == "__main__":
+    generar_reporte_comisiones()
+
+
+    
+
